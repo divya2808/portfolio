c15b07838f52eedbfcccacc2b6dcfd06f13a783e
kandlakd <divya.kandlakunta@wwt.com>
2021-01-06 21:29:26 -0500

    About page

---
+++ pages/about.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="bg-black h-screen w-screen flex items-center justify-center">
+    <div class="flex justify-center flex-col">
+      <div class="text-white text-3xl text-center">Hello!</div>
+      <div class="my-4 text-center">
+        <p class="text-white text-lg">My name is Divya Kandlakunta. I'm a full stack web developer.</p>
+        <p class="text-white text-lg">Building creative and elegant websites, solving problems and providing innovative and efficient solutions is my passion</p>
+        <p class="text-white text-lg">If you are seeking web presense or an employer looking to hire please reach out to me</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
